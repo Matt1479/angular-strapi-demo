9efce745feb941a7ed5ad28d4627d7a8ea6bc531
Matt1479 <gitmatt1479@gmail.com>
2022-05-17 09:39:21 +0200

    feature(global): display api pages in PagesComponent

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { map, Observable } from 'rxjs';
-import { Page, StrapiDTO } from './pages.model';
 
 @Component({
   selector: 'app-root',
@@ -9,26 +6,8 @@ import { Page, StrapiDTO } from './pages.model';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'angular-demo';
-  pagesDTO$: Observable<StrapiDTO> = null as any;
-  pages$: Observable<Page[]> = null as any;
+  title = 'angular-app';
+  constructor() {}
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.pagesDTO$ = this.http.get<StrapiDTO>(
-      'http://localhost:1337/api/pages'
-    );
-    this.pages$ = this.pagesDTO$.pipe(
-      map((dto: StrapiDTO) =>
-        dto.data.map((value) => {
-          let { title, content } = value.attributes;
-          return {
-            title,
-            content,
-          };
-        })
-      )
-    );
-  }
+  ngOnInit() {}
 }
